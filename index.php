315ecfa04fb19775036f9245857f785224c70387
ipakey <bornagainyfke@gmai.com>
2021-10-27 17:52:30 +0100

    favicon added

--- index.php
+++ index.php
@@ -6,9 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header Flex from scratch</title>
     <link rel="stylesheet" href="./css/style.css">
-    
+    <link href="./assets/branding/theden_favicon.ico" rel="shortcut icon" type="image/x-icon" />
+
+
 </head>
+
+
 <body>
-    
+<?php include_once './partPages/header.php';  
+?>
+
 </body>
 </html>
---
+++ partPages/header.php
@@ -0,0 +1,5 @@
+/* Header container code  */ 
+
+<div class="container">
+    This is my header page
+</div>
